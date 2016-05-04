a0acc69caaef1e658051bffd45d84abaf2ecf17f
alberto__segura <alberto.segura.delgado@gmail.com>
2016-05-04 23:06:20 +0200

    añadido parseo de fichero de peliculas

---
+++ P4/codigo/src/Movies.h
@@ -0,0 +1,67 @@
+
+
+// To read files
+#include <fstream>
+
+#include <stdio.h>
+#include <iostream>
+
+#include <vector>
+#include <map>
+
+
+class Movies
+{
+
+private:
+    std::map<int, std::string> movies;
+    int numMovies;
+
+public:
+
+    Movies()
+    {
+        numMovies = 0;
+    }
+
+    void readFile(std::string filename)
+	{
+		std::fstream file(filename.c_str(), std::ios_base::in);
+
+		if(!file)
+		{
+			throw std::runtime_error("Error: Invalid filename");
+		}
+
+        int movieid = 0;
+        char actChar[512];
+        std::string name;
+
+		do{
+
+            file >> movieid;
+            file.getline(actChar, 512);
+
+            std::string str(actChar);
+            str = str.substr(1); // delete first |
+            name = str.substr( 0, str.find("|") ); // to end name
+
+            movies[movieid] = name; // save movie
+            numMovies++;
+
+            //std::cout << "MID: " << movieid << " NAME: " << name << std::endl;
+
+        }while(!file.eof());
+	}
+
+    std::string getMovie(int mid)
+    {
+        return movies[mid];
+    }
+
+    int getMovies()
+    {
+        return numMovies;
+    }
+
+};
--- P4/codigo/src/main.cpp
+++ P4/codigo/src/main.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 
 #include "Ratings.h"
+#include "Movies.h"
 
 int main(int argc, char **argv)
 {
@@ -12,7 +13,12 @@ int main(int argc, char **argv)
 
     myRatings.readFile("./data/ua.base");
 
-    std::cout << myRatings.getRating(1, 10) << std::endl;
-    std::cout << myRatings.getUsers() << " : " << myRatings.getMovies() << std::endl;
+    std::cout << "Rating for user 1 movie 10: " << myRatings.getRating(1, 10) << std::endl;
+    std::cout << "Readed users&movies: " << myRatings.getUsers() << " : " << myRatings.getMovies() << std::endl;
+
+    Movies movies;
+    movies.readFile("./data/u.item");
+    std::cout << "Movie 3: "  << movies.getMovie(3) << std::endl;
+    std::cout << "Readed movies: "  << movies.getMovies() << std::endl;
 
 }
